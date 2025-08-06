0dd5eb440e437425ea7bef7affc7e84d8e2dd157
xloc <xloc.cc@outlook.com>
2025-08-05 20:31:31 -0700

    project scaffold

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-screen flex flex-col justify-center items-center gap-2">
+
+    <button class="rounded-lg bg-slate-200 hover:bg-slate-100 p-2">Connect</button>
+    <pre class="bg-slate-100 w-100 p-2 rounded-lg">{{ result }}</pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+
+const result = ref('hello');
+</script>
